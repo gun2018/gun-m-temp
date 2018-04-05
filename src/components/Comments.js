0e6755ce3c187ac37e326d19e9089c5cfec99895
viyam22 <1020793720@qq.com>
2018-04-06 00:02:12 +0800

    feat: 添加comments组件

---
+++ src/components/Comments.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+class Comments extends PureComponent {
+  render() {
+    return (
+      <Warp>
+        <p>评论</p>
+        <input />
+        <div></div>
+      </Warp>
+    )
+  }
+}
+export default Comments;
