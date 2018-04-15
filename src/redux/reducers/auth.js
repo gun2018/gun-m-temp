4766f0eda031188b89d0c72e1b8134545983dd14
陆文 <ervendai@gmail.com>
2018-04-15 18:32:22 +0800

    feat: 增加 redux 模拟登陆数据

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,10 +1,11 @@
 import { UPDATE_USER_INFO } from '../actionTypes';
 
 const initialState = {
-  openId: '',
-  headimgurl: '',
-  nickname: '',
-  sex: 0,
+  openId: 'o0-zO0meBBqLalUgpYzG06XO7wAg',
+  headimgurl:
+    'http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKiboeh23vhCNjJTxlXbs9mLiagiczy8Wd6gIxviaV4vpUyOso741Qz53XDdKyAkTH4Iic7SPkkax8xO0g/132',
+  nickname: '好像叫陆文',
+  sex: 1,
 };
 
 const auth = (state = initialState, { type, payload = {} }) => {
