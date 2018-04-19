3b7cf27984183a11f2bd08dc67bc65dc18288edf
viyam22 <1020793720@qq.com>
2018-04-19 23:54:22 +0800

    feat: 完善首页样式

--- src/views/index/PostList.js
+++ src/views/index/PostList.js
@@ -2,7 +2,7 @@ import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Icon, Layout } from 'antd';
+import { Icon, Layout, Avatar } from 'antd';
 
 import { fromNow } from '../../utils/date';
 import px2rem from '../../styles/px2rem';
@@ -11,7 +11,7 @@ const Post = styled(Link)`
   color: #444444;
   display: inline-block;
   border-bottom: 1px solid #ebebeb;
-  height: ${px2rem(640)};
+  height: ${px2rem(570)};
   width: 100%;
   border-bottom: 1px #efeff5 solid;
   margin-bottom: ${px2rem(70)};
@@ -19,7 +19,29 @@ const Post = styled(Link)`
 const Content = styled(Layout)`
   background-color: #fff;
   width: ${px2rem(620)};
-  height: 100%;
+  > .info {
+    width: 100%;
+    height: ${px2rem(108)};
+    position: relative;
+    padding: ${px2rem(32)} 0 0 ${px2rem(164)};
+    > .avatar {
+      position: absolute;
+      top: 0;
+      left: ${px2rem(44)};
+      transform: translateY(-50%);
+      width: ${px2rem(80)};
+      height: ${px2rem(80)};
+      border-radius: 50%;
+    }
+    > .name {
+      font-size: ${px2rem(28)};
+      color: #212128;
+    }
+    > .time {
+      font-size: ${px2rem(24)};
+      color: #B2B2B2;
+    }
+  }
 `;
 const Sider = styled(Layout)`
   width: ${px2rem(130)};
@@ -29,6 +51,7 @@ const Sider = styled(Layout)`
   font-size: ${px2rem(28)};
   color: #c7c7d3;
   background-color: #fff;
+  text-align: center;
   > .icon {
     font-size: ${px2rem(32)};
     margin-bottom: ${px2rem(40)};
@@ -38,7 +61,7 @@ const Sider = styled(Layout)`
   }
   > .icon-more {
     font-size: ${px2rem(32)};
-    margin-top: ${px2rem(180)};
+    margin-top: ${px2rem(120)};
     transform: rotate(90deg);
   }
 `;
@@ -59,9 +82,16 @@ const CoverWrap = styled.div`
     background-color: rgba(255, 255, 255, 0.7);
     position: absolute;
     bottom: 0;
-    color: #575656;
-    padding-left: ${px2rem(188)};
-    font-size: ${px2rem(28)};
+    padding: ${px2rem(12)} ${px2rem(40)} 0 ${px2rem(188)};
+    > h3 {
+      color: #575656;
+      font-size: ${px2rem(28)};
+      height: 100%;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
   }
 `;
 
@@ -80,17 +110,20 @@ class PostList extends PureComponent {
         {posts.map(post => (
           <Post key={post.id} to={`/post?post_id=${post.id}`}>
             <Layout hasSider="true" className="post-item">
-              <Content className="content">
-                <div>
-                  <CoverWrap>
-                    <img src={post.cover} alt="cover" />
-                    <div className="title">
-                      <h3>{post.title}</h3>
-                    </div>
-                  </CoverWrap>
-                  <p className="breaf">{post.brief}</p>
-                  {/* <span>{fromNow(post.createTime)}</span> */}
+              <Content>
+                <CoverWrap>
+                  <img src={post.cover} alt="cover" />
+                  <div className="title">
+                    <h3>{post.title}</h3>
+                  </div>
+                </CoverWrap>
+                <div className="info">
+                  <Avatar icon="user" className="avatar" />
+                  <div className="name">李大狗</div>
+                  <div className="time">6小时前</div>
                 </div>
+                {/* <p className="breaf">{post.brief}</p> */}
+                {/* <span>{fromNow(post.createTime)}</span> */}
               </Content>
               <Sider className="sider" breakpoint="sm">
                 <Icon type="heart" className="icon" />
